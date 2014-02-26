e07bb4059fdfd2a4b658bfc9ee0f714df35156fd
Arnab Deka <arnab.deka@gmail.com>
2014-02-26 15:33:37 +0530

    smaller named functions.

--- clojure/word-count/word-count.clj
+++ clojure/word-count/word-count.clj
@@ -10,10 +10,13 @@
 (defn- filter-words [str]
   (filter non-blank? (tokenize str)))
 
+(defn- add-count-for [word counts]
+  (assoc counts word (inc (get counts word 0))))
+
 (defn- count [words counts]
   (if (empty? words)
     counts
-    (count (rest words) (assoc counts (first words) (inc (get counts (first words) 0))))))
+    (count (rest words) (add-count-for (first words) counts))))
 
 (defn word-count [str]
   (count (filter-words str) {}))
