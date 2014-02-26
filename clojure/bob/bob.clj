00cdd89f75c905c2fb4ab97824a969ae1f73bbee
Arnab Deka <arnab.deka@gmail.com>
2014-02-26 06:04:25 +0530

    completed bob.clj

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -1,6 +1,18 @@
-(ns bob)
+(ns bob
+  (:use [clojure.string :only [join blank?]]))
+
+(defn- all-caps? [str]
+  (let [ascii-chars (join "" (re-seq #"[a-zA-Z]" str))]
+    (and
+     (not (blank? ascii-chars))
+     (every? #(Character/isUpperCase %) ascii-chars))))
+
+(defn- question? [str]
+  (re-matches #".*\?\Z" str))
 
 (defn response-for [str]
   (cond
-   (re-matches #"?$" str)
+   (blank? str) "Fine. Be that way!"
+   (all-caps? str) "Woah, chill out!"
+   (question? str) "Sure."
    :else "Whatever."))
