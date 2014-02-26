37ab21183987e851ba587e1864255c324be694ee
Arnab Deka <arnab.deka@gmail.com>
2014-02-26 14:59:12 +0530

    RNA to DNA.

--- clojure/rna-transcription/dna.clj
+++ clojure/rna-transcription/dna.clj
@@ -1,6 +1,21 @@
-(ns dna)
+(ns dna
+  (:use [clojure.string :only [join]]))
+
+(def dna-to-rna {"G" "C", "C" "G", "T" "A", "A" "U"})
+
+(defn- valid-dna-strand? [strand]
+  (contains? dna-to-rna strand))
+
+(defn- validate-dna-seq [strand]
+  ;; clojure.contrib.except/throw-if-not would be suitable
+  (when-not (valid-dna-strand? strand)
+    (throw (AssertionError.
+            (format "Unknown DNA strand: %s" strand)))))
+
+(defn- validate-and-transcribe [strand-as-char]
+  (let [strand (str strand-as-char)]
+    (validate-dna-seq strand)
+    (dna-to-rna strand)))
 
 (defn to-rna [dna-seq]
-  (let
-      [dna-to-rna {"G" "C", "C" "G", "T" "A", "A" "U"}]
-    (dna-to-rna dna-seq)))
+  (join "" (map validate-and-transcribe dna-seq)))
