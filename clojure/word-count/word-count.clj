df35ccb0ed6e079127a5b63a8ab5f567c999fe07
Arnab Deka <arnab.deka@gmail.com>
2014-02-28 22:52:20 +0530

    replace a whole lot of frequency calc code with... frequencies :)

--- clojure/word-count/word-count.clj
+++ clojure/word-count/word-count.clj
@@ -7,13 +7,5 @@
 (defn- filter-words [str]
   (filter (comp not blank?) (tokenize str)))
 
-(defn- add-count-for [word counts]
-  (assoc counts word (inc (get counts word 0))))
-
-(defn- count [words counts]
-  (if (empty? words)
-    counts
-    (count (rest words) (add-count-for (first words) counts))))
-
 (defn word-count [str]
-  (count (filter-words str) {}))
+  (frequencies (filter-words str)))
