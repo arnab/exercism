f89e763c78eed2c41815b6135276c8c39e7b1a87
Arnab Deka <arnab.deka@gmail.com>
2014-04-11 17:39:38 +0530

    working beer.

--- clojure/beer-song/beer.clj
+++ clojure/beer-song/beer.clj
@@ -1,60 +1,28 @@
 (ns beer
   (:use [clojure.string :only [join]]))
 
-;; (def phrase-tmpl
-;;   ["%s of beer on the wall, %s of beer."
-;;    "Take one down and pass it around, %s of beer on the wall."
-;;    ""])
-
-;; (defn- count-things [count things-meta]
-;;   (let [word (get things-meta :word "")]
-;;     (cond
-;;      (= 0 count) (str count " " word "s")
-;;      (= 1 count) (str count " " word)
-;;      :else (str count " " word "s"))))
-
-;; (defn verse [n]
-;;   (let [current-count (count-things n {:word "bottle"})
-;;         next-count    (count-things (dec n) {:word "bottle"})]
-;;     (format
-;;      (join "\n" phrase-tmpl)
-;;      current-count
-;;      current-count
-;;      next-count)))
-
-(defn- pluralize [n word]
+(defn- first-stanza [n]
   (cond
-   (= 0 n) (str "no more " word "s")
-   (= 1 n) (str n " " word)
-   :else (str n " " word "s"))  )
+   (= n 0) "No more bottles of beer on the wall, no more bottles of beer.\n"
+   (= n 1) (str n " bottle of beer on the wall, " n " bottle of beer.\n")
+   :else (str n " bottles of beer on the wall, " n " bottles of beer.\n")))
 
-(defn- current-count-verse [n]
-  (let [count n
-        pluralized-count (pluralize n "bottle")]
-    (str pluralized-count " of beer on the wall, "
-         pluralized-count " of beer.")))
+(defn- second-stanza [n]
+  (cond
+   (= n 0) "Go to the store and buy some more, "
+   (= n 1) "Take it down and pass it around, "
+   :else "Take one down and pass it around, "))
 
-(defn- count-to-take-down [n]
-  (if (= n 1) "it" "one"))
-
-(defn- next-action-verse [n]
-  (if (= n 0)
-    (str "Go to the store and buy some more")
-    (str "Take "
-         (count-to-take-down n)
-         " down and pass it around")))
-
-(nth (cycle (range 99 -1 -1)) 100)
-
-(defn- next-count-verse [n]
-  (str (pluralize (dec n) "bottle")
-       " of beer on the wall."))
+(defn- last-stanza [n]
+  (cond
+   (= n -1) "99 bottles of beer on the wall.\n"
+   (= n 0) "no more bottles of beer on the wall.\n"
+   (= n 1) (str n " bottle of beer on the wall.\n")
+   :else (str n " bottles of beer on the wall.\n")))
 
 (defn verse [n]
   (str
-   (current-count-verse n) "\n"
-   (next-action-verse n) ", "
-   (next-count-verse n) "\n"))
+   (first-stanza n) (second-stanza n) (last-stanza (dec n))))
 
 (defn sing
   ([from] (sing from 0))
