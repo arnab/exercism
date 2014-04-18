35efef4b1aa8d0542bdc78bd1fb9a0531a61ffcf
Arnab Deka <arnab.deka@gmail.com>
2014-04-18 15:52:36 +0530

    simplify distance calculation even more.

--- clojure/point-mutations/dna.clj
+++ clojure/point-mutations/dna.clj
@@ -2,10 +2,4 @@
 
 (defn hamming-distance
   [s1 s2]
-  (loop [[hd1 & tl1] s1
-         [hd2 & tl2] s2
-         acc-dist (atom 0)]
-    (if-not (= hd1 hd2) (swap! acc-dist inc))
-    (if (some empty? [tl1 tl2])
-      @acc-dist
-      (recur tl1 tl2 acc-dist))))
+  (count (filter identity (map not= s1 s2))))
