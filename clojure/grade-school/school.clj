0ed82cd6f683a8857faee6c5a0dbcd1391a50c0e
Arnab Deka <arnab.deka@gmail.com>
2014-04-18 17:54:00 +0530

    sort maps (keys and values)

--- clojure/grade-school/school.clj
+++ clojure/grade-school/school.clj
@@ -7,3 +7,13 @@
 (defn grade
   [db grade]
   (get db grade []))
+
+(defn- transform-values-in
+  [m f]
+  "walk kv sets and aply f on every v"
+  (into {} (for [[k v] m] [k (f v)])))
+
+(defn sorted
+  [db]
+  (into (sorted-map)
+        (transform-values-in db sort)))
