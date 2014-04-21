e06ca1589ca86e61b62d76c4000592385a8bc782
Arnab Deka <arnab.deka@gmail.com>
2014-04-21 19:52:26 +0530

    remove duplication by using update-in instead of constructing the list again.

--- clojure/robot-name/robot.clj
+++ clojure/robot-name/robot.clj
@@ -1,11 +1,11 @@
 (ns robot)
 
 (def id-seq (ref 0))
-(def name-seq (ref (list "A" "A" 0)))
+(def name-seq (ref ["A" "A" 0]))
 (def robots (ref {}))
 
 (defn- inc-name-seq [curr-seq]
-  (list "A" "A" (inc (last curr-seq))))
+  (update-in curr-seq [2] inc))
 
 (defn- format-name [name]
   (apply format "%s%s%03d" name))
