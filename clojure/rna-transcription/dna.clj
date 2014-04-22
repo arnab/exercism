7283e25b07840d01cb6c33b4d3574e0bb7dd70da
Arnab Deka <arnab.deka@gmail.com>
2014-04-22 16:35:48 +0530

    Simplify validation by using case instead of seperate methods. Thanks @caulagi (via exercism).

--- clojure/rna-transcription/dna.clj
+++ clojure/rna-transcription/dna.clj
@@ -1,21 +1,14 @@
 (ns dna
   (:use [clojure.string :only [join]]))
 
-(def dna-to-rna {"G" "C", "C" "G", "T" "A", "A" "U"})
-
-(defn- valid-dna-strand? [strand]
-  (contains? dna-to-rna strand))
-
-(defn- validate-dna-seq [strand]
-  ;; clojure.contrib.except/throw-if-not would be suitable
-  (when-not (valid-dna-strand? strand)
+(defn- validate-and-transcribe [strand]
+  (case strand
+    \G \C
+    \C \G
+    \T \A
+    \A \U
     (throw (AssertionError.
             (format "Unknown DNA strand: %s" strand)))))
-
-(defn- validate-and-transcribe [strand-as-char]
-  (let [strand (str strand-as-char)]
-    (validate-dna-seq strand)
-    (dna-to-rna strand)))
 
 (defn to-rna [dna-seq]
   (join "" (map validate-and-transcribe dna-seq)))
