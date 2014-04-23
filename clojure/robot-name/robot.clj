f46762586efcf2c57c46f430056be613e4cae2b4
Arnab Deka <arnab.deka@gmail.com>
2014-04-24 01:48:58 +0530

    reduce a lot of complexity by treating robots as indivisual atoms...
    ... instead of keeping a mutable list of robots (like you would in
    Ruby).

--- clojure/robot-name/robot.clj
+++ clojure/robot-name/robot.clj
@@ -1,9 +1,6 @@
 (ns robot)
 
-(def id-seq (ref 0))
 (def name-seq (ref ["A" "A" 0]))
-(def robots (ref {}))
-
 (defn- inc-name-seq [curr-seq]
   (update-in curr-seq [2] inc))
 
@@ -13,37 +10,16 @@
 (defn- generate-name []
   (format-name @name-seq))
 
-(defn- generate-name-and-inc-seq []
+(defn- next-name []
   (let [name (generate-name)]
     (dosync (alter name-seq inc-name-seq))
     name))
 
-(defn- build []
-  {:name (generate-name)
-   :id @id-seq})
-
-(defn- manufacture []
-  (let [new-robot (build)]
-    (dosync
-     (alter robots assoc @id-seq new-robot)
-     (alter name-seq inc-name-seq)
-     (alter id-seq inc))
-    new-robot))
-
-(defn- change-name [robot new-name]
-  (let [id (robot :id)]
-    (dosync
-     (alter robots assoc-in [id :name] new-name))
-    (@robots id)))
-
 (defn robot []
-  (manufacture))
+  (atom (next-name)))
 
 (defn robot-name [robot]
-  (or (:name ( @robots (robot :id)))
-      (:name (change-name robot (generate-name-and-inc-seq)))))
+  @robot)
 
 (defn reset-name [robot]
-  (let [original-name (robot :name)]
-    (change-name robot nil)
-    original-name))
+  (reset! robot (next-name)))
