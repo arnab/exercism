1b6d85b04105545a5d59129927ea2139878af633
Arnab Deka <arnab.deka@gmail.com>
2014-04-29 18:50:29 +0530

    ability to find days of a specific name in a month, using clj-time.

--- clojure/meetup/meetup.clj
+++ clojure/meetup/meetup.clj
@@ -1,13 +1,28 @@
-(ns meetup)
-(import 'java.util.Calendar)
+(ns meetup
+  (:require [clj-time.core :as t]
+            [clj-time.predicates :as tpr]))
 
-(defn- dates-in [yr mm day-name]
-  "returns a list of dates, given the year and month and day-name"
-  )
+(defn- dates-in
+  "returns a list of dates, given the year and month and day-name.
+   e.g. All dates in 2013 Feb: (dates-in 2014 4)
+   All Mondays in 2013 Feb: (dates-in 2014 4 1)"
+  ([yr mm]
+     (loop [d (t/date-time yr mm)
+            dates []]
+       (if (= mm (t/month d))
+         (recur (t/plus d (t/days 1)) (conj dates d))
+         dates)))
+  ([yr mm dow]
+     (filter
+      #(= dow (t/day-of-week %))
+      (dates-in yr mm))))
 
-(defn- schedule-by-week [dates pos]
-  "returns the date from the list by it's position. nil if position is not in the list"
-  )
+(defn- schedule-by-week [yr mm dow pos]
+  "returns the date from the list by it's position.
+   nil if position is not in the list.
+   e.g. Fourth Sunday of March 2013:
+          (schedule-by-week 2013 3 7 4)"
+  (get (dates-in yr mm dow) (dec pos)))
 
 (defn- schedule-by-range [dates date-range]
   "returns the first date that's also in the given range. nil if none match"
@@ -18,14 +33,3 @@
 
 ;;; get last
 ;; (get v (dec (count v)))
-
-(let [c (Calendar/getInstance)]
-  (doto c
-    (.set Calendar/YEAR 2013)
-    (.set Calendar/MONTH 1) ; Feb is 1
-    ;; (.set Calendar/DAY_OF_MONTH 1)
-    ;; (.set Calendar/DAY_OF_WEEK Calendar/SATURDAY)
-    )
-  [(.getActualMinimum c Calendar/DAY_OF_MONTH)
-   (.getActualMaximum c Calendar/DAY_OF_MONTH)
-   c])
