9eff84af02d7f44ae891061378b066a5fc7d5225
Arnab Deka <arnab.deka@gmail.com>
2014-05-07 22:42:37 +0530

    all done. Love higher level functions and macros!

--- clojure/space-age/space_age.clj
+++ clojure/space-age/space_age.clj
@@ -1,9 +1,26 @@
 (ns space-age)
 
-(def orbital-period-ratios { :mercury 0.2408467 })
+(def orbital-period-ratios
+  {:mercury 0.2408467
+   :venus 0.61519726
+   :mars 1.8808158
+   :jupiter 11.862615
+   :saturn 29.447498
+   :uranus 84.016846
+   :neptune 164.79132})
 
 (defn on-earth [n]
   (/ n 31557600.0))
 
-(defn on-mercury [n]
-  (/ (on-earth n) (orbital-period-ratios :mercury)))
+(defn- on-planet [p]
+  (fn [n]
+    (/ (on-earth n)
+       (orbital-period-ratios p))))
+
+(defmacro on-planet-fn [p]
+  `(intern *ns*
+           (symbol (str "on-" (name ~p)))
+           (on-planet ~p)))
+
+(doseq [p (keys orbital-period-ratios)]
+  (on-planet-fn p))
