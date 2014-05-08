ac48290837009fe0dc3b3c74a0f7cf4215293ef4
Arnab Deka <arnab.deka@gmail.com>
2014-05-08 21:38:08 +0530

    memoize grain calculation.

--- clojure/grains/grains.clj
+++ clojure/grains/grains.clj
@@ -1,10 +1,11 @@
 (ns grains)
 
-(defn square [num]
+(defn- square-unmemoized [num]
   (loop [n num acc 1]
    (if (= n 1)
       acc
       (recur (dec n) (*' 2 acc)))))
+(def square (memoize square-unmemoized))
 
 (defn total []
   (let [squares (map square (range 1 65))]
