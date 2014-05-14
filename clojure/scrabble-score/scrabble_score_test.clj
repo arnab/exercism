3ccb13144032ac2bcf0c808012b8ee4b74af00e6
Arnab Deka <arnab.deka@gmail.com>
2014-05-14 17:54:38 +0530

    scrabble example.

---
+++ clojure/scrabble-score/scrabble_score_test.clj
@@ -0,0 +1,19 @@
+(ns scrabble.test (:require [clojure.test :refer :all]))
+(load-file "scrabble.clj")
+
+(deftest lower-case-letter
+  (is (= 1 (scrabble/score-letter "a"))))
+(deftest upper-case-letter
+  (is (= 1 (scrabble/score-letter "A"))))
+(deftest two-letter-word
+  (is (= 2 (scrabble/score-word "at"))))
+(deftest bigger-word-1
+  (is (= 6 (scrabble/score-word "street"))))
+(deftest bigger-word-2
+  (is (= 22 (scrabble/score-word "quirky"))))
+(deftest all-upper-case-word
+  (is (= 20 (scrabble/score-word "MULTIBILLIONAIRE"))))
+(deftest all-mixed-case-cabbage
+  (is (= 14 (scrabble/score-word "Cabbage"))))
+
+(run-tests)
