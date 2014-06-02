b70d8e872edea4ea588673690af6512d9d8ae818
Arnab Deka <arnab.deka@gmail.com>
2014-06-02 16:11:02 +0530

    solve using one check (not conds) and destructuring.

--- clojure/scrabble-score/scrabble.clj
+++ clojure/scrabble-score/scrabble.clj
@@ -1,21 +1,22 @@
 (ns scrabble)
 
-(defn score-letter
-  "scrabble score for a letter"
-  [letter]
-  (let [l (clojure.string/lower-case letter)]
-    (cond
-     (contains? #{"a" "e" "i" "o" "u" "l" "n" "r" "s" "t"} l) 1
-     (contains? #{"d" "g"} l) 2
-     (contains? #{"b" "c" "m" "p"} l) 3
-     (contains? #{"f" "h" "v" "w" "y"} l) 4
-     (contains? #{"k"} l) 5
-     (contains? #{"j" "x"} l) 8
-     (contains? #{"q" "z"} l) 10
-     :else (throw (IllegalArgumentException. (str "Unknown letter: " l)))
-     )))
+(let [scores [[#{"a" "e" "i" "o" "u" "l" "n" "r" "s" "t"} 1]
+              [#{"d" "g"} 2]
+              [#{"b" "c" "m" "p"} 3]
+              [#{"f" "h" "v" "w" "y"} 4]
+              [#{"k"} 5]
+              [#{"j" "x"} 8]
+              [#{"q" "z"} 10]]]
 
-(defn score-word
-  "scrabble score for a word."
-  [[& letters]]
-  (reduce + (map score-letter letters)))
+  (defn score-letter
+    "scrabble score for a letter"
+    [letter]
+    (let [l (clojure.string/lower-case letter)]
+      (loop [[[letters & [score]] & more-scores] scores]
+        (if (contains? letters l) score
+            (recur more-scores)))))
+
+  (defn score-word
+    "scrabble score for a word."
+    [[& letters]]
+    (reduce + (map score-letter letters))))
