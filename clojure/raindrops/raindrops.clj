3674a4183da39a37b7326dfc675c79a9f975c324
Arnab Deka <arnab.deka@gmail.com>
2014-06-03 16:41:42 +0530

    reuse factors from the previous exercise.

--- clojure/raindrops/raindrops.clj
+++ clojure/raindrops/raindrops.clj
@@ -2,22 +2,13 @@
   (require [clojure.set :as set :only [intersection]]
            [clojure.string :as s :only [join]]))
 
-(let [divisible? (comp zero? rem)]
-  (defn- factors
-    "returns the prime factors of n"
-    [n]
-    (loop [n n
-           candidate 2
-           acc []]
-      (if (<= n 1) acc
-          (if (divisible? n candidate)
-            (recur (bigint (/ n candidate)) candidate (conj acc candidate))
-            (recur n (inc candidate) acc))))))
+;;; from the previous exercise: http://git.io/3ziOAQ
+(load-file "../prime-factors/prime_factors.clj")
 
 (let [lang {3 "Pling" 5 "Plang" 7 "Plong"}]
   (defn convert
     [n]
-    (let [ns (set (factors n))]
-      (if-let [found-ns (seq (set/intersection ns (set (keys lang))))]
+    (let [factors (set (prime-factors/of n))]
+      (if-let [found-ns (seq (set/intersection factors (set (keys lang))))]
         (s/join (map lang found-ns))
         (str n)))))
