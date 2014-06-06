61f307e9e5515ce59e3007aa649947f9ef59a8e3
Arnab Deka <arnab.deka@gmail.com>
2014-06-06 18:01:08 +0530

    recatored code to be simpler, more concise.

--- clojure/allergies/allergies.clj
+++ clojure/allergies/allergies.clj
@@ -9,17 +9,15 @@
 (defn list
   "list of allergies for the given score"
   [score]
-  (loop [s score
-         allergy-num (dec (count allergies))
+  (loop [s score                        ; remaining score
+         n (dec (count allergies))      ; potential allergy, position
          acc []]
-    (cond
-     (<= s 0) acc
-     (< allergy-num 0) acc
-     :else (let [allergy-score (Math/pow 2 allergy-num)
-                 allergy (nth allergies allergy-num)]
-             (if (>= s allergy-score)
-               (recur (- s allergy-score) allergy-num (cons allergy acc))
-               (recur s (dec allergy-num) acc))))))
+    (if (or (zero? s) (neg? n)) (distinct acc)
+        (let [allergy-score (Math/pow 2 n)
+              allergy (nth allergies n)]
+          (if (>= s allergy-score)
+            (recur (- s allergy-score) n (cons allergy acc))
+            (recur s (dec n) acc))))))
 
 (defn allergic_to?
   "returns whether the given score includes the allergy"
