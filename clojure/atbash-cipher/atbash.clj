8660354c69a061ddf0827736fafc57a4bcec9b2f
Arnab Deka <arnab.deka@gmail.com>
2014-06-07 17:09:03 +0530

    thread the fns for better readability.

--- clojure/atbash-cipher/atbash.clj
+++ clojure/atbash-cipher/atbash.clj
@@ -1,15 +1,19 @@
 (ns atbash
-  (require [clojure.string :as str :only [lower-case]]))
+  (require [clojure.string :as string :only [lower-case]]))
 
 (let [letters (map char (range
                          (int \a)
                          (inc (int \z))))
-      total-letters (dec (count letters))
-      ]
-  (defn transpose
+      total-letters (dec (count letters))]
+  (defn- transpose
     [c]
-    (nth letters (- total-letters
-                    (.indexOf letters c))))
+    (->> c
+         (.indexOf letters)
+         (- total-letters)
+         (nth letters)))
   (defn encode
     [s]
-    (apply str (map transpose (str/lower-case s)))))
+    (->> s
+         string/lower-case
+         (map transpose)
+         (apply str))))
