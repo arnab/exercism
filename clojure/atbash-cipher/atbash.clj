ee17e7976cf55e4430de6d77fefa12ec0f7441f2
Arnab Deka <arnab.deka@gmail.com>
2014-06-08 07:25:14 +0530

    renamed vars

--- clojure/atbash-cipher/atbash.clj
+++ clojure/atbash-cipher/atbash.clj
@@ -1,19 +1,23 @@
 (ns atbash
-  (require [clojure.string :as string :only [lower-case]]))
+  (require [clojure.string :as string :only [lower-case join]]))
 
-(let [letters (map char (range
-                         (int \a)
-                         (inc (int \z))))
-      total-letters (dec (count letters))]
+(let [alphabet (map #(str (char %)) (range
+                                    (int \a)
+                                    (inc (int \z))))
+      alphabet-set (set alphabet)
+      total (dec (count alphabet))]
   (defn- transpose
     [c]
-    (->> c
-         (.indexOf letters)
-         (- total-letters)
-         (nth letters)))
+    (if (contains? alphabet-set c)
+      (let [complement (- total (.indexOf alphabet c))]
+        (nth alphabet complement))
+      c))
   (defn encode
     [s]
     (->> s
          string/lower-case
+         (re-seq #"\w")
          (map transpose)
-         (apply str))))
+         (partition 5 5 [])
+         (map #(apply str %))
+         (string/join " "))))
